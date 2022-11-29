f0fcb13e7af532de2da6110ee6be4459c30ce6a3
Alexmachine1 <meklex1@gmail.com>
2022-11-29 14:41:58 +0100

    jjj

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:75] + " " + [115:119] = " " + [:5]
+str = str[39:75] + [-19:-15] + [:6]
 print(str)
